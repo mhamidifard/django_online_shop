38e6f33d40ab70d0ea415b4b93c3dfb3170ff475
mohammad <m.hamidifard82@gmail.com>
2025-07-12 21:17:42 +0330

    add JWT authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
-# Create your views here.
+
+@api_view(['GET'])
+def test_authentication(request:Request, format=None):
+    if request.user.is_authenticated:
+        return Response({'message': 'You are logged in'}, status=status.HTTP_200_OK)
+    return Response({'message': 'You are not logged in'},status=status.HTTP_403_FORBIDDEN)
--- online_shop/urls.py
+++ online_shop/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView, TokenVerifyView,
+)
+
+from accounts.views import test_authentication
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('test-authentication/',test_authentication)
+
 ]
